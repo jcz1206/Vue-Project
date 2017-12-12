7c39655553e5ac9a4c09fea3d7f209cdf2826718
jcz1206 <jcz1206@126.com>
2017-12-12 17:30:50 +0800

    index json active

--- src/components/index.vue
+++ src/components/index.vue
@@ -13,14 +13,14 @@
                 </div>
             </header>
             <!--喇叭-->
-            <div class="trumpt">
+            <div class="trumpt"  v-if="data!=null">
                 <div class="col-1 dsmall">
                     <i class="icon iconfont icon-shengyin"></i>
                 </div>
                 <div id="scroll_div" class="gg2 col-10">
                     <div id="scroll_begin">
                         <ul id="au">
-                            <li v-for="item in trumpt">
+                            <li v-for="item in data.trumpt" >
                                 <a href="http://www.SuperSlide2.com" target="_blank">
                                     <p>{{item.title}}</p>
                                 </a>
@@ -34,25 +34,25 @@
                 </div>
             </div>
             <!--轮播图-->
-            <div class="swiper-container">
+            <div class="swiper-container"  v-if="data!=null">
                 <div class="swiper-wrapper" style="z-index:4;background-color: rgba(0,0,0,0);">
-                    <a v-for="item in advert" :href="item.url" class="swiper-slide">
+                    <a v-for="item in data.advert" :href="item.url" class="swiper-slide" >
                         <span :style="'background-image:url('+item.img+')'"></span>
                     </a>
                 </div>
                 <div class="swiper-pagination"></div>
             </div>
             <!--分类-->
-            <div class="category">
-                <div v-for="item in category">
+            <div class="category"  v-if="data!=null">
+                <div v-for="item in data.category">
                     <a :href="item.url">
                         <div :style="'background-image:url('+item.img+')'"></div>
                         <span>{{item.title}}</span>
                     </a>
                 </div>
             </div>
-            <!--优惠活动-->
-            <div class="txtMarquee-left">
+            <!--头条-->
+            <div class="txtMarquee-left"  v-if="data!=null">
                 <div class="hd">
                     <span></span>
                     <!-- <a class="next"></a>
@@ -61,7 +61,7 @@
                 <div class="bd">
                     <div class="tempWrap" style="overflow:hidden; position:relative; ">
                         <ul class="infoList">
-                            <li v-for="item in trumpt">
+                            <li v-for="item in data.trumpt">
                                 <a href="http://www.SuperSlide2.com" target="_blank">{{item.title}}</a>
                                 <span></span>
                             </li>
@@ -69,7 +69,7 @@
                     </div>
                 </div>
             </div>
-
+            <!--活动-->
             <div>
             </div>
         </div>
@@ -91,113 +91,131 @@ export default {
     footbar
   },
   data() {
-    
-    //引入的vue.js 直接data:{}赋值
-    return require('json/index.json')
-    //  this.$http.get('api').then(function (data){
-    //     return data;
-    //   }, function (){
-    //     alert(2)
-    //   });
-    // return {
-    //   trumpt: [
-    //     { title: "1快递盒子千万别扔2", url: "", type: 0 },
-    //     { title: "3华为华为华为哈弗哈弗哈弗哈弗哈弗4", url: "", type: 0 },
-    //     { title: "5全面屏全面屏全面屏全面屏全面屏全面屏全面屏全面屏全面屏全面屏6", url: "", type: 0 },
-    //     {
-    //       title: "7飞猪旅行飞猪旅行飞猪旅行飞猪旅行飞猪旅行飞猪旅行飞猪旅行飞猪旅行飞猪旅行飞猪旅行飞猪旅行8",
-    //       url: "",
-    //       type: 0
+    return {
+      data: null
+    //     data: {
+    //           trumpt: [
+    //             { title: "1快递盒子千万别扔2", url: "", type: 0 },
+    //             { title: "3华为华为华为哈弗哈弗哈弗哈弗哈弗4", url: "", type: 0 },
+    //             { title: "5全面屏全面屏全面屏全面屏全面屏全面屏全面屏全面屏全面屏全面屏6", url: "", type: 0 },
+    //             {
+    //               title: "7飞猪旅行飞猪旅行飞猪旅行飞猪旅行飞猪旅行飞猪旅行飞猪旅行飞猪旅行飞猪旅行飞猪旅行飞猪旅行8",
+    //               url: "",
+    //               type: 0
+    //             }
+    //           ],
+    //           advert: [
+    //             { img: require("@/assets/images/banner1.jpg"), url: "#", type: 0 },
+    //             { img: require("@/assets/images/banner2.jpg"), url: "#", type: 0 },
+    //             { img: require("@/assets/images/banner3.jpg"), url: "#", type: 0 },
+    //             { img: require("@/assets/images/banner4.jpg"), url: "#", type: 0 }
+    //           ],
+    //           category: [
+    //             {
+    //               title: "天猫",
+    //               img: require("@/assets/images/category/1.png"),
+    //               url:
+    //                 "https://www.tmall.com/?locate=icon-1&spm=a215s.7406091.icons.1&scm=2027.1.2.1000&force=m&wh_from=tbc#/main",
+    //               type: 0
+    //             },
+    //             {
+    //               title: "聚划算",
+    //               img: require("../assets/images/category/2.png"),
+    //               url:
+    //                 "https://jhs.m.taobao.com/m/index.htm?locate=icon-2&spm=a215s.7406091.icons.2&scm=2027.1.2.1001",
+    //               type: 0
+    //             },
+    //             {
+    //               title: "天猫国际",
+    //               img: require("../assets/images/category/3.png"),
+    //               url:
+    //                 "https://pages.tmall.com/wow/import/17151/tmallglobal?locate=icon-3&spm=a215s.7406091.icons.3&scm=2027.4.1.16",
+    //               type: 0
+    //             },
+    //             {
+    //               title: "外卖",
+    //               img: require("../assets/images/category/4.png"),
+    //               url:
+    //                 "https://market.m.taobao.com/apps/pm/paimai/index.html?wh_weex=true&wx_navbar_transparent=true&data_prefetch=true&locate=icon-9&spm=a215s.7406091.icons.9&scm=2027.1.2.10060000",
+    //               type: 0
+    //             },
+    //             {
+    //               title: "天猫超市",
+    //               img: require("../assets/images/category/5.png"),
+    //               url:
+    //                 "https://chaoshi.m.tmall.com/?locate=icon-5&spm=a215s.7406091.icons.5&scm=2027.1.2.1007&_ig=shoutao",
+    //               type: 0
+    //             },
+    //             {
+    //               title: "天猫",
+    //               img: require("@/assets/images/category/1.png"),
+    //               url: "",
+    //               type: 0
+    //             },
+    //             {
+    //               title: "聚划算",
+    //               img: require("../assets/images/category/2.png"),
+    //               url: "",
+    //               type: 0
+    //             },
+    //             {
+    //               title: "天猫国际",
+    //               img: require("../assets/images/category/3.png"),
+    //               url: "",
+    //               type: 0
+    //             },
+    //             {
+    //               title: "外卖",
+    //               img: require("../assets/images/category/4.png"),
+    //               url: "",
+    //               type: 0
+    //             },
+    //             {
+    //               title: "天猫超市",
+    //               img: require("../assets/images/category/5.png"),
+    //               url: "",
+    //               type: 0
+    //             }
+    //           ],
+    //           activity: []
     //     }
-    //   ],
-    //   advert: [
-    //     { img: require("@/assets/images/banner1.jpg"), url: "#", type: 0 },
-    //     { img: require("@/assets/images/banner2.jpg"), url: "#", type: 0 },
-    //     { img: require("@/assets/images/banner3.jpg"), url: "#", type: 0 },
-    //     { img: require("@/assets/images/banner4.jpg"), url: "#", type: 0 }
-    //   ],
-    //   category: [
-    //     {
-    //       title: "天猫",
-    //       img: require("@/assets/images/category/1.png"),
-    //       url:
-    //         "https://www.tmall.com/?locate=icon-1&spm=a215s.7406091.icons.1&scm=2027.1.2.1000&force=m&wh_from=tbc#/main",
-    //       type: 0
-    //     },
-    //     {
-    //       title: "聚划算",
-    //       img: require("../assets/images/category/2.png"),
-    //       url:
-    //         "https://jhs.m.taobao.com/m/index.htm?locate=icon-2&spm=a215s.7406091.icons.2&scm=2027.1.2.1001",
-    //       type: 0
-    //     },
-    //     {
-    //       title: "天猫国际",
-    //       img: require("../assets/images/category/3.png"),
-    //       url:
-    //         "https://pages.tmall.com/wow/import/17151/tmallglobal?locate=icon-3&spm=a215s.7406091.icons.3&scm=2027.4.1.16",
-    //       type: 0
-    //     },
-    //     {
-    //       title: "外卖",
-    //       img: require("../assets/images/category/4.png"),
-    //       url:
-    //         "https://market.m.taobao.com/apps/pm/paimai/index.html?wh_weex=true&wx_navbar_transparent=true&data_prefetch=true&locate=icon-9&spm=a215s.7406091.icons.9&scm=2027.1.2.10060000",
-    //       type: 0
-    //     },
-    //     {
-    //       title: "天猫超市",
-    //       img: require("../assets/images/category/5.png"),
-    //       url:
-    //         "https://chaoshi.m.tmall.com/?locate=icon-5&spm=a215s.7406091.icons.5&scm=2027.1.2.1007&_ig=shoutao",
-    //       type: 0
-    //     },
-    //     {
-    //       title: "天猫",
-    //       img: require("@/assets/images/category/1.png"),
-    //       url: "",
-    //       type: 0
-    //     },
-    //     {
-    //       title: "聚划算",
-    //       img: require("../assets/images/category/2.png"),
-    //       url: "",
-    //       type: 0
-    //     },
-    //     {
-    //       title: "天猫国际",
-    //       img: require("../assets/images/category/3.png"),
-    //       url: "",
-    //       type: 0
-    //     },
-    //     {
-    //       title: "外卖",
-    //       img: require("../assets/images/category/4.png"),
-    //       url: "",
-    //       type: 0
-    //     },
-    //     {
-    //       title: "天猫超市",
-    //       img: require("../assets/images/category/5.png"),
-    //       url: "",
-    //       type: 0
-    //     }
-    //   ],
-    //   activity: []
-    // };
+    };
   },
+  created:{
+
+  },
+  computed: {},
   mounted: function() {
-    // this.share();
-    this.$nextTick(function() {
-      this.swiperTool();
-      this.marqueeTool();
-      this.scrollTrumpt();
-    });
+      this.getData();
+    // this.$nextTick(function() {
+    //   this.swiperTool();
+    //   this.marqueeTool();
+    //   // this.scrollTrumpt();
+    // });
+  },
+  watch:{
+
   },
   methods: {
     // share:function(){
     //   $('#share-3').share();
     // }
+    getData: function() {
+      this.$http.get("/static/test.json").then(
+        response => {
+          this.data = response.data;
+          this.$nextTick(function() {
+            this.swiperTool();
+            this.marqueeTool();
+            this.scrollTrumpt();
+          });
+        },
+        response => {
+          // 响应错误回调
+          alert("服务器请求失败");
+        }
+      );
+    },
     search: () => {
       layer.alert("搜索内容是" + $(event.currentTarget).val());
     },
