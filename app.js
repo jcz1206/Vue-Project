ea468302c8e0a43f214cdf0e59b8fd2cd2ef27bc
jcz1206 <jcz1206@126.com>
2018-01-29 17:34:21 +0800

    1

--- app.js
+++ app.js
@@ -9,9 +9,20 @@ app.listen(3000,() => {
 })  
 */
 
-const express = require('express')
 const path = require('path')
+const express = require('express')
 const app = express()
+
+//myself add two start
+var cookieParser = require("cookie-parser");
+var session = require('express-session');
+app.use(cookieParser('sessiontest'));
+app.use(session({
+    secret: 'sessiontest', //与cookieParser中的一致
+    resave: true,
+    saveUninitialized: true
+}));
+//myself add two end
 
 app.use(express.static(path.join(__dirname, 'dist')))
 app.listen(3000, () => {
@@ -62,16 +73,63 @@ apiRoutes.get("/user2/*", function(req, res) {
     res.json({ result: flag }); // res.json(userData)
 })
 
+
 function checkUserLogin(un, pass) {
-    var flag = false;
-    userData.forEach((user) => {
-        if (user.username === un && user.password === pass) {
-            flag = true;
-            return flag;
+    var flag = 0; //账号不存在
+    var user1 = null;
+    for (var i = 0; i < userData.length; i++) {
+        var user0 = userData[i];
+        if (user0.username === un && user0.password === pass) {
+            flag = 1; //账号存在
+            user1 = user0;
+            break;
         }
-    })
-    return flag;
+    }
+    // userData.forEach((user0) => {
+    //     if (user0.username === un && user0.password === pass) {
+    //         flag = 1; //账号存在
+    //         user1 = user0;
+    //         break;//foreach中的break报错
+    //         // return { retCode: flag, user: user0, c: { name: user0.username } };
+    //     }
+    // })
+    return { retCode: flag, user: user1 };
+
 }
+
+//请求地址写法是/user 参数是post方式传递 username=xxx,password=
+apiRoutes.post("/login", function(req, res) {
+    var result = checkUserLogin(req.body.username, req.body.password);
+    if (result.retCode == 1) {
+        req.session.user = result.user;
+    }
+    res.json(result); // res.json(userData)
+})
+
+//请求地址写法是/user 参数是post方式传递 username=xxx,password=
+apiRoutes.post("/checklogin", function(req, res, next) {
+    if (req.session.user) {
+        res.json({ retCode: 1, user: req.session.user }); //已登录
+    } else {
+        // var result = checkUserLogin(req.body.username, req.body.password);
+        res.json({ retCode: 0, user: null }); //未登录
+        // res.json(userData)
+    }
+})
+
+apiRoutes.post("/loginOut", function(req, res) {
+    req.session.destroy(function(err) {
+        if (err) {
+            res.json({ retCode: 0, retMsg: '退出登录失败' });
+            return;
+        }
+        // req.session.loginUser = null;
+        // res.clearCookie(identityKey);
+        // res.redirect('/newlogin');
+        res.json({ retCode: 1, retMsg: '退出登录成功' });
+    });
+})
+
 
 app.use(apiRoutes);
 
